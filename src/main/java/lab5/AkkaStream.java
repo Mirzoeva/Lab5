d0b847f22f4b42b7174431ff8e140d4f0e3a9b0e
Mirzoeva <vselennaya314@gmail.com>
2019-11-28 14:29:39 +0300

    Scripted auto-commit on change (2019-11-28 14:29:39) by gitwatch.sh

---
+++ src/main/java/lab5/AkkaStream.java
@@ -0,0 +1,46 @@
+package lab5;
+
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import java.util.concurrent.CompletionStage;
+import scala.concurrent.Future;
+import static akka.http.javadsl.server.PathMatchers.segment;
+
+
+public class AkkaStream{
+//    public static void main(String[] args) throws IOException {
+//        System.out.println("start!");
+//        ActorSystem system = ActorSystem.create("routes");
+//        final Http http = Http.get(system);
+//        final ActorMaterializer materializer =
+//                ActorMaterializer.create(system);
+//        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow =
+////        <вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+//        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+//                routeFlow,
+//                ConnectHttp.toHost("localhost", 8080),
+//                materializer
+//        );
+//        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+//        System.in.read();
+//        binding
+//                .thenCompose(ServerBinding::unbind)
+//                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+//    }
+}
+
+
+   
