1c42858f91ee3e48da38f12c966751e0742bb654
Mirzoeva <vselennaya314@gmail.com>
2019-11-30 02:21:28 +0300

    Scripted auto-commit on change (2019-11-30 02:21:28) by gitwatch.sh

--- src/main/java/lab5/Tester.java
+++ src/main/java/lab5/Tester.java
@@ -1,4 +1,21 @@
 package lab5;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import org.asynchttpclient.AsyncHttpClient;
+
 public class Tester {
+    private final ActorMaterializer materializer;
+    private final ActorRef storage;
+    private final AsyncHttpClient asyncHttpClient;
+
+    public Tester(AsyncHttpClient asyncHttpClient, ActorSystem system, ActorMaterializer materializer){
+        this.asyncHttpClient = asyncHttpClient;
+        this.materializer = materializer;
+        this.storage = system.actorOf();
+
+    }
+
+
 }
