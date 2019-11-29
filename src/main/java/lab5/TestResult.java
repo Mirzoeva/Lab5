1e401960559d1acb8c593a865362b564cdfcf2e6
Mirzoeva <vselennaya314@gmail.com>
2019-11-30 02:52:42 +0300

    Scripted auto-commit on change (2019-11-30 02:52:42) by gitwatch.sh

--- src/main/java/lab5/TestResult.java
+++ src/main/java/lab5/TestResult.java
@@ -1,4 +1,19 @@
 package lab5;
 
 public class TestResult {
+    private final UrlTest test;
+    private final Long avg;
+
+    public TestResult(UrlTest test, Long avg){
+        this.test = test;
+        this.avg = avg;
+    }
+
+    public UrlTest qetTest(){
+        return test;
+    }
+
+    public Long getAvg(){
+        return avg;
+    }
 }
