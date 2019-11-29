a60be3bd0bbe179782788c88f73f155645cd50ff
Mirzoeva <vselennaya314@gmail.com>
2019-11-30 02:49:06 +0300

    Scripted auto-commit on change (2019-11-30 02:49:06) by gitwatch.sh

--- src/main/java/lab5/UrlTest.java
+++ src/main/java/lab5/UrlTest.java
@@ -1,7 +1,27 @@
 package lab5;
 
+import javax.script.Invocable;
+
 public class UrlTest implements Comparable<UrlTest>{
     private final String url;
-    private final int count;
+    private final Integer count;
+
+    public UrlTest(String url, Integer count){
+        this.url = url;
+        this.count = count;
+    }
+
+    public String getUrl(){
+        return url;
+    }
+
+    public int getCount(){
+        return count;
+    }
     
+    @Override
+    public int compareTo(UrlTest b){
+        return this.url.compareTo(b.url) != 0 ?  this.url.compareTo(b.url) : this.count.compareTo(b.count);
+
+    }
 }
