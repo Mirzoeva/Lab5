738bca1250692ed7fe3f447ea167b763dca32eb3
Mirzoeva <vselennaya314@gmail.com>
2019-11-30 06:18:17 +0300

    Scripted auto-commit on change (2019-11-30 06:18:17) by gitwatch.sh

--- src/main/java/lab5/Constants.java
+++ src/main/java/lab5/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     public static int countOfRequests = 10;
     public static String url = "testUrl";
     public static String count = "count";
-
+    static int port = 8080;
 
 }
