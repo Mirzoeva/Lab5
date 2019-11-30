024be3c90291cd47d915599a98660d115c4dd7c7
Mirzoeva <vselennaya314@gmail.com>
2019-11-30 03:05:21 +0300

    Scripted auto-commit on change (2019-11-30 03:05:21) by gitwatch.sh

--- src/main/java/lab5/ReturnUrlTestResult.java
+++ src/main/java/lab5/ReturnUrlTestResult.java
@@ -1,4 +1,15 @@
 package lab5;
 
+import java.util.Optional;
+
 public class ReturnUrlTestResult {
+    private final TestResult result;
+
+    public ReturnUrlTestResult(TestResult result){
+        this.result = result;
+    }
+
+    public Optional<TestResult> get(){
+        return result.getAvg() != null ? Optional.of(result) : Optional.empty();
+    }
 }
